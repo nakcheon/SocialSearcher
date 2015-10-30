f435188a7b6408871aad0d358bab1dace4225b0f
ncjung <asdfghjkl1000@hanmail.net>
2015-10-30 15:36:36 +0900

    Initial Commit

---
+++ SocialSearcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialSearcher
+//
+//  Created by NakCheonJung on 10/30/15.
+//  Copyright © 2015 ncjung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialSearcher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocialSearcher
+//
+//  Created by NakCheonJung on 10/30/15.
+//  Copyright © 2015 ncjung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
